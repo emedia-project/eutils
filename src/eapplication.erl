a70a369c4c6b3a5a9b40e89791d88935fd441c2e
Sebastien Ferre <sebastien@ferre.io>
2014-12-30 18:20:41 +0100

    Added function to set app env

--- src/eapplication.erl
+++ src/eapplication.erl
@@ -1,7 +1,41 @@
 -module(eapplication).
 
--export([get_env/3]).
+-export([
+	get_env/3,
+	set_env_from_file/1,
+  set_env_from_config/1,
+  set_env/2
+       ]).
 
 % @deprecated Use application:get_env/3
 get_env(App, Key, Default) ->
   application:get_env(App, Key, Default).
+
+%% @doc
+%% @end
+-spec set_env_from_file(file:filename()) -> ok | {error, any()}.
+set_env_from_file(File) ->
+  case file:consult(File) of
+    {ok, [Terms]} ->
+      set_env_from_config(Terms);
+    E ->
+      E
+  end.
+
+%% @doc
+%% @end
+-spec set_env_from_config([term()]) -> ok | {error, any()}.
+set_env_from_config([]) -> ok;
+set_env_from_config([{AppName, AppConfig}|Rest]) ->
+  case set_env(AppName, AppConfig) of
+    ok -> set_env_from_config(Rest);
+    E -> E
+  end.
+
+%% @doc
+%% @end
+-spec set_env(atom(), [term()]) -> ok | {error, any()}.
+set_env(AppName, []) -> ok;
+set_env(AppName, [{Key, Value}|Config]) ->
+  _ = application:set_env(AppName, Key, Value),
+  set_env(AppName, Config).
